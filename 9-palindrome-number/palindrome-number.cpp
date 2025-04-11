7453e6c3dc700f155cae98db24254b586ef9b610
Janvi Chaudhary <janvichaudhary1211@gmail.com>
2025-04-11 20:25:14 +0530

    Time: 8 ms (12.37%) | Memory: 10.8 MB (16.49%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0)
+          return false;
+        string num = to_string(x);
+       int i=0;
+       int j=num.length()-1;
+       while(i<=j){
+        if(num[i]==num[j]){
+            i++;
+            j--;
+        }else{
+        return false;
+        }
+       }
+       return true;
+    }
+};
